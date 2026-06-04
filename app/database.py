19d85f9159c54e90a9497efeeaa3adf81a3175a3
albertosar29-jpg <albertosar29@gmail.com>
2026-06-04 00:22:54 -0500

    fix: remove duplicate imports and DATABASE_URL in database.py

--- app/database.py
+++ app/database.py
@@ -1,12 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 import os
 
@@ -14,7 +7,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-print("DATABASE_URL =", DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
@@ -28,7 +20,10 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
-
+    try:
+        yield db
+    finally:
+        db.close()
     try:
         yield db
     finally:
